4e3c64f77b8ac0f3ef6bb1088f2cbd27164d5333
Jairus312 <jairus.didde@gmai.com>
2025-04-16 13:49:19 +0530

    O(1)Time

---
+++ Data structures/Lesson_2/O(1)Time.py
@@ -0,0 +1,10 @@
+def printnumber(n):
+    iteration = 0
+    print("The number entered by the user is", n)
+    iteration += 1
+    print("Total iterations done by the code is",iteration,"/n") 
+
+x = (int(input("Enter the Number : ")))
+printnumber(x)
+
+print("\n with any'n' the time taken by our code won't change" )
