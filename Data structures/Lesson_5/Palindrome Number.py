1f78adf84579ddd053418317d26ca58665bb018d
Jairus312 <jairus.didde@gmai.com>
2025-04-21 13:00:47 +0530

    Palindrome Number

---
+++ Data structures/Lesson_5/Palindrome Number.py
@@ -0,0 +1,14 @@
+Number = int(input("Enter the Number :-"))
+original_number = Number
+reversed_number = 0
+
+while Number > 0:
+    digit = Number % 10 
+    reversed_number = reversed_number * 10 + digit
+    Number //= 10
+
+if original_number == reversed_number:
+    print(f"{original_number} is a panlindrome.")
+
+else:
+    print(f"{original_number} is not a panlindrome.")
