749dba4bff9b516467fd5684cfc83511cf1d1596
Jairus312 <jairus.didde@gmai.com>
2025-04-24 16:11:25 +0530

    FirstSetBit

---
+++ Data structures/Lesson_8/FirstSetBit.py
@@ -0,0 +1,11 @@
+def FirstSetBit(number):
+    position = 1
+    mask = 1
+    while(not(number & mask)):
+        mask = mask << 1
+        position += 1
+
+    return position 
+number = int(input("enter your number :"))
+print("position of the first set bit : ",FirstSetBit(number))
+1
