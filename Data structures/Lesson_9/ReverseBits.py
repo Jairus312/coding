b40ebf31c76a4fd99186f42eebf6af75fdeda540
Jairus312 <jairus.didde@gmai.com>
2025-04-29 13:32:53 +0530

    PowerOf2

---
+++ Data structures/Lesson_9/ReverseBits.py
@@ -0,0 +1,9 @@
+def reverseBits (number): 
+    reversed = 0 
+    reversed = reversed <<< 1  
+    if (number & 1 = 1): 
+    reversed reversed1 
+    number = number >> 1 
+    return reversed 
+number = int(input("Enter your number: ")) 
+print("Number with reversed bits: ", reverseBits (number))
