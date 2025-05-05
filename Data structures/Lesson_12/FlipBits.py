153f5a58c87c4431d7b85786431ee72ebd7c8db4
Jairus312 <jairus.didde@gmai.com>
2025-05-05 18:07:04 +0530

    1to10Recursion

--- Data structures/Lesson_12/FlipBits.py
+++ Data structures/Lesson_12/FlipBits.py
@@ -6,7 +6,7 @@ def flipstotals(number1, number2):
         if(t1 != t2):
             flips += 1
 
-        number1 >>1= 1
+        number1 >>= 1
         number2 >>= 1
     return flips
 number1 = int(input("Enter First number : "))
---
+++ Data structures/Lesson_12/Sub srings.py
@@ -0,0 +1,4 @@
+import math;
+
+def printPowersrping((set,setsize))
+    printPowersrping = (int) (math.pow(2,setsize))
