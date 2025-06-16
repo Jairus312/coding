83fd1a8d485c4c94828fc8b2e3106427a60cbee7
Jairus312 <jairus.didde@gmai.com>
2025-06-16 18:09:06 +0530

    ReverseArrayGroup

---
+++ Data structures/Lesson_20/ReverseArrayGroup.py
@@ -0,0 +1,20 @@
+def reverse(a, a_size, n):
+    temp = 0
+    while(temp < a_size):
+
+        start = temp
+
+        end = min(temp + n - 1, a_size - 1)
+        while(start < end):
+            a[start], a[end] = a[end], a[start]
+            start += 1;
+            end -+ 1
+        temp += 1
+
+a = [5,23,5,23,123,5,136,7,56]
+n = 2
+a_size = len(a)
+reverse(a,a_size, n)
+
+for i in range(0, a_size):
+    print(a[i], end=" ")
