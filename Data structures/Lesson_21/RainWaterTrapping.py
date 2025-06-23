dba0327f610addecc64ba823a40fcbcf31056bd3
Jairus312 <jairus.didde@gmai.com>
2025-06-23 18:13:19 +0530

    Maximum1s

--- Data structures/Lesson_21/RainWaterTrapping.py
+++ Data structures/Lesson_21/RainWaterTrapping.py
@@ -1,13 +1,30 @@
-def findwater(a, a_size):
-    leftTallest = [0]*a_size
-    rightTallest = [0]*a_size
-    water = 0
-    leftTallest[0] = a[0]
-    for i in range(1, a_size):
-        leftTallest[i] = max(leftTallestTallest[i-1],a[i])
+# program to find the amount of water that we can trap within a given set of bars
 
-    rightTallest[0] = a[0]
-    for i in range(1, a_size):
-        rightTallest[i] = max(rightTallest[i+1],a[i])
-            
-    for i in range(0, a_size)  
+def findwater(a, a_size):
+
+  # make array to hold the height of left tallest bar for any ith bar lefttallest = [0]*a_size 
+
+  # make array to hold the height of the right tallest bar for any ith bar righttallest = [0]*a_size
+
+  # initialize result
+  water = 0
+
+  # fill left array
+  lefttallest[0] = a(0)
+  for i in range( 1, a_size):
+    lefttallest[i] = max(lefttallest[i-1], a[i])
+
+  # fill right array
+  righttallest[a_size-1] = a[a_size -1]
+  for i in range(a_size -2, -1, -1):
+    righttallest[i] = max(righttallest[i +1], a[i])
+
+  # water trapped for any ith bar should be minium of the left and right highest bar - bar height
+  for i in range(0, a_size):
+    water += min(lefttallest[i], righttallest[i]) - a[i]
+    
+  return water 
+
+a = [0,1,0,2,1,0,1,3,2,1,2,1]
+bars = len(a)
+print("water : ", findwater(a, bars))
